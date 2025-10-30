3bdee238a9d9547100d2d297d0cfec149993b43d
Pablo Galilea <pgalilea@me.com>
2025-10-30 20:56:07 -0300

    feat: add sqlite sync and async

--- memx/memory/sqlite.py
+++ memx/memory/sqlite.py
@@ -12,20 +12,12 @@ from memx.memory import BaseMemory
 
 
 class SQLiteMemory(BaseMemory):
-    table_sql = """
-        CREATE TABLE IF NOT EXISTS '{table_name}' (
-            session_id TEXT,
-            message JSONB,
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        );
-        CREATE INDEX IF NOT EXISTS session_index ON '{table_name}' (session_id);
-    """
-
     def __init__(self, uri: str, table: str, session_id: str = None):
-        self.table_name = table.strip()
+        self.table_name = f"'{table.strip()}'"
+        self.init_queries()
 
         self.async_engine = create_async_engine(uri, echo=False, future=True)
-        self.AsyncSessionLocal = async_sessionmaker(
+        self.AsyncSessionCtx = async_sessionmaker(
             bind=self.async_engine,
             expire_on_commit=False,
             class_=AsyncSession,
@@ -38,7 +30,7 @@ class SQLiteMemory(BaseMemory):
             connect_args={"check_same_thread": True},
         )
 
-        self.SyncSessionLocal = sessionmaker(
+        self.SyncSessionCtx = sessionmaker(
             autocommit=False,
             autoflush=False,
             bind=self.sync_engine,
@@ -46,21 +38,16 @@ class SQLiteMemory(BaseMemory):
         )
 
         with self.sync_engine.begin() as conn:
-            conn.connection.executescript(
-                self.table_sql.format(table_name=self.table_name)
-            )
+            conn.connection.executescript(self.table_sql)
+
+        self.sync = _sync(self)  # to group sync methods
 
         if session_id:
             self._session_id = session_id
         else:
             self._session_id = str(uuid4())
 
-    def add(self, messages: list[dict]):
-        insert_sql = f"""
-            INSERT INTO '{self.table_name}' (session_id, message, created_at)
-            VALUES (:session_id, :message, :created_at);
-        """
-
+    async def add(self, messages: list[dict]):
         ts_now = datetime.now(timezone.utc)
         data = {
             "session_id": self._session_id,
@@ -68,23 +55,68 @@ class SQLiteMemory(BaseMemory):
             "created_at": ts_now,
         }
 
-        with self.SyncSessionLocal() as session:
-            session.execute(text(insert_sql), data)
+        async with self.AsyncSessionCtx() as session:
+            await session.execute(text(self.insert_sql), data)
+            await session.commit()
+
+    async def get(self) -> list[dict]:
+        async with self.AsyncSessionCtx() as session:
+            result = await session.execute(
+                text(self.get_sql),
+                {"session_id": self._session_id},
+            )
+
+        result = [dict(row._mapping) for row in result.fetchall()]
+        for i in range(len(result)):
+            result[i]["message"] = orjson.loads(result[i]["message"])
+
+        return result
+
+    def init_queries(self):
+        """."""
+
+        self.table_sql = dedent(f"""
+            CREATE TABLE IF NOT EXISTS {self.table_name} (
+                session_id TEXT,
+                message JSONB,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
+            CREATE INDEX IF NOT EXISTS session_index ON {self.table_name} (session_id);
+        """)
+
+        self.insert_sql = dedent(f"""
+            INSERT INTO {self.table_name} (session_id, message, created_at)
+            VALUES (:session_id, :message, :created_at);
+        """)
+
+        self.get_sql = dedent(f"""
+            SELECT * FROM {self.table_name}
+            WHERE session_id = :session_id 
+            ORDER BY created_at ASC;
+        """)
+
+
+class _sync(BaseMemory):
+    def __init__(self, parent: "SQLiteMemory"):
+        self.pm = parent  # parent memory (?)
+
+    def add(self, messages: list[dict]):
+        ts_now = datetime.now(timezone.utc)
+        data = {
+            "session_id": self.pm._session_id,
+            "message": json.dumps(messages),
+            "created_at": ts_now,
+        }
+
+        with self.pm.SyncSessionCtx() as session:
+            session.execute(text(self.pm.insert_sql), data)
             session.commit()
 
     def get(self) -> list[dict]:
-        with self.SyncSessionLocal() as session:
+        with self.pm.SyncSessionCtx() as session:
             result = session.execute(
-                text(
-                    dedent(
-                        f"""
-                        SELECT * FROM '{self.table_name}'
-                        WHERE session_id = :session_id 
-                        ORDER BY created_at ASC;
-                        """
-                    )
-                ),
-                {"session_id": self._session_id},
+                text(self.pm.get_sql),
+                {"session_id": self.pm._session_id},
             )
 
         result = [dict(row._mapping) for row in result.fetchall()]
